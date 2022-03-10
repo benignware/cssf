86e688e2f6cb2f8c897e3fb9f5acc13fb17e08d6
Rafael Nowrotek <rexblack@users.noreply.github.com>
2022-03-10 22:05:38 +0100

    Expose sass proxy

--- int/sass/functions.js
+++ int/sass/functions.js
@@ -1,65 +1,100 @@
-const { Color } = require('chroma-js');
-const chroma = require('chroma-js');
 const sass = require('sass');
+const cssf = require('cssf');
 
-const {
-  hsla,
-  rgba,
-  hue,
-  saturation,
-  lightness,
-  red,
-  green,
-  blue,
-  darken,
-  mix,
-} = require('../../lib');
+const decamelize = (string) =>
+  string.replace(/^[a-z]|[A-Z]/g, (c, i) => (i ? '-' : '') + c.toLowerCase());
 
-const getArgs = (...args) =>
-  args.map((arg) => (String(arg) !== 'null' ? String(arg) : undefined));
+function getParams(func) {
+  const args = func.toString().match(/function\s.*?\(([^)]*)\)/)[1];
 
-const fnProxy = (fn) =>
-  function (...args) {
-    args = getArgs(...args);
-    console.log('**** CALL FN: ', `${fn.name}(${args.join(', ')})`);
+  return args
+    .split(',')
+    .map(function (arg) {
+      return arg.replace(/\/\*.*\*\//, '').trim();
+    })
+    .filter(function (arg) {
+      return arg;
+    })
+    .map((param) => {
+      const [, name, value] = param.match(/([\w\d]+)(?:\s*=\s*(.*))?/) || [];
 
-    try {
-      const result = fn(...args);
+      return {
+        name,
+        value:
+          typeof value === 'string'
+            ? value.replace(/^['"]/, '').replace(/['"]$/, '')
+            : value,
+      };
+    });
+}
 
-      console.log('**** FN RESULT: ', result);
+const unit = (value) => {
+  if (!isNaN(Number(value))) {
+    return '';
+  }
 
-      if (typeof result === 'number' || !isNaN(parseFloat(result))) {
-        console.log('NUMBER TYPE');
-        return new sass.types.Number(result);
-      }
-
-      try {
-        const color = chroma(result);
-
-        return new sass.types.Color(...color.rgba())
-      } catch (e) {
-        console.log('no color');
-      }
-
-      return new sass.types.String(result);
-    } catch (error) {
-      console.log('ERR', error);
-    }
-
-    return sass.types.Null.NULL;
-  };
-
-const functions = {
-  'f-hsla($h, $s: null, $l:null, $a: null)': fnProxy(hsla),
-  'f-rgba($h, $s: null, $l:null, $a: null)': fnProxy(rgba),
-  'f-hue($color)': fnProxy(hue),
-  'f-saturation($color)': fnProxy(saturation),
-  'f-lightness($color)': fnProxy(lightness),
-  'f-red($color)': fnProxy(red),
-  'f-green($color)': fnProxy(green),
-  'f-blue($color)': fnProxy(blue),
-  'f-darken($color, $weight: null)': fnProxy(darken),
-  'f-mix($color1, $color2, $weight: null)': fnProxy(mix),
+  return (String(value).match(/^s*[-\d.e]+([a-z%]{2,})/) || [])[1] || '';
 };
 
-module.exports = functions;
+const getArgs = (...args) => {
+  return args.map((arg) => {
+    if (arg instanceof sass.types.Color) {
+      return `rgba(${arg.getR()}, ${arg.getG()}, ${arg.getB()}, ${arg.getA()})`;
+    }
+
+    if (arg instanceof sass.types.Number) {
+      return arg.getUnit()
+        ? `${arg.getValue()}${arg.getUnit()}`
+        : arg.getValue();
+    }
+
+    if (arg instanceof sass.types.String) {
+      return arg.getValue();
+    }
+
+    return String(arg) !== 'null' ? String(arg) : undefined;
+  });
+};
+
+const fnProxy = (fn) => {
+  return function (...args) {
+    args = getArgs(...args);
+    const result = fn(...args);
+
+    if (typeof result === 'number' || !isNaN(parseFloat(result))) {
+      const u = unit(result);
+
+      return new sass.types.Number(parseFloat(result), u);
+    }
+
+    return new sass.types.String(result);
+  };
+};
+
+const libProxy = (lib, options = {}) => {
+  const { prefix = '' } = options;
+
+  return Object.assign(
+    {},
+    ...Object.values(lib).map((fn) => {
+      const key = `${prefix}${decamelize(fn.name)}(${getParams(fn)
+        .map(
+          ({ name, value }) =>
+            `$${name}${
+              typeof value !== 'undefined'
+                ? `: ${value.replace(/^\s*{/, '(').replace(/}\s*$/, ')')}`
+                : ''
+            }`
+        )
+        .join(', ')})`;
+
+      return {
+        [key]: fnProxy(fn),
+      };
+    })
+  );
+};
+
+module.exports = libProxy(cssf, {
+  prefix: 'cssf-',
+});
